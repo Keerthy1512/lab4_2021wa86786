a4d1e0bd64528846e143210a941defeafcf24986
cloud@2021wa86786 <2021wa86786@wilp.bits-pilani.ac.in>
2025-07-16 14:54:00 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,9 @@
 public class HelloWorld {
   public static void main (String[] args) {
     System.out.println("Hello, world_2021wa86786!");
-    System.out.println(commit_change1");
-    System.out.println(commit_change2");
+    System.out.println("commit_change1";
+    System.out.println("commit_change1");
+    
+  
   }
 }
